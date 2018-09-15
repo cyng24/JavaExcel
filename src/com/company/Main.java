fbe89f48367ad8fdaf3e14ca4731a05730b0c579
cyng24 <cyng24@gmail.com>
2018-09-15 18:56:09 +0200

    added switch command statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
@@ -6,6 +7,35 @@ public class Main {
         int numberOfColumns = 14;
         int numberOfRows = 12;
 
-        Grid.printGrid(numberOfColumns, numberOfRows);
+        boolean running = true;
+        while(running) {
+            System.out.print("Command? ");
+            Scanner myScanner = new Scanner(System.in);
+            String command = myScanner.nextLine();
+
+            switch (command) {
+                case "print":
+                    Grid.printGrid(numberOfColumns, numberOfRows);
+                    break;
+                case "display":
+                    System.out.println("Which cell?");
+                    break;
+                case "set value":
+                    System.out.println("What value?");
+                    break;
+                case "clear":
+                    System.out.println("Which cell?");
+                    break;
+                case "sort":
+                    System.out.println("Ascending or descending?");
+                    break;
+                case "quit":
+                    System.out.println("Thanks for playing!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Not a command.");
+            }
+        }
     }
 }
